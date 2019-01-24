11d08de1cecc767f7a62dc288bcd46ec26e676bc
Chiptune <modchiptune@gmail.com>
2019-01-24 20:40:35 +0900

    LIST

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var el={};
+var el={},audio_player,audio_meta={};
 addEventListener("message",msg);
 function msg(e){
 	var d=e.data;
@@ -24,4 +24,19 @@ function msg(e){
 		};
 		audio_player.onstop=state_stop;
 	}
+}
+function t_toHref(a){
+	return a.toLowerCase().replace(/[^a-z0-9_\s]/g,"").replace(/[\s-]+/g,"-").replace(/^[\s\W]+|[\s\W]+&/,"")
+}
+function t_toFileName(a){
+	return a.replace(/[\\\/\*?"<>|]/g,"").trim();
+}
+function hmsToSeconds(d){
+	d=[0,0].concat(d.split(":")).splice(-3);
+	return (d[0]|0)*3600+(d[1]|0)*60+(d[2]|0);
+}
+function secondsToHms(d){
+	d=Number(d);
+	var h=Math.floor(d/3600),m=Math.floor(d%3600/60),s=Math.floor(d%3600%60);
+	return ("0"+h).slice(-2)+":"+("0"+m).slice(-2)+":"+("0"+s).slice(-2);
 }
