02f0cf05a0fd595f66a4f7bd53b1e29acf5a257d
Chiptune <modchiptune@gmail.com>
2019-01-24 20:26:35 +0900

    LIST

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-onmessage=function(e){
-	var a=e.data,b;
-	if(a[0]=="init"){
-		b=document.createElement("script");
-		b.src=a[1];
-		document.head.appendChild(b);
+addEventListener("message",msg);
+function msg(e){
+	var d=e.data;
+	if(d[0]=="init-cdn"){
+		removeEventListener("message",msg);
+
+		el["lib-svg"].innerHTML=d[1];
+
+		mod.chiptune.config.html_control=d[3];
+
+		var a=document.createElement("script");
+		a.textContent=d[2];
+		document.head.appendChild(a);
+
+		mod.chiptune.config.url_worker=URL.createObjectURL(d[4]);
+		mod.chiptune.config.url_libopenmpt=d[5];
+		mod.chiptune.config.url_libopenmpt_wasm=URL.createObjectURL(d[6]);
+
+		audio_player=mod.chiptune_control.new(".chip-control");
+		audio_player.onplay=function(){
+			state_play(audio_meta.url,audio_meta.native);
+		};
+		audio_player.onstop=state_stop;
 	}
-};
+}
