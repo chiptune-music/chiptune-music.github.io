4168036088f9d9876adb55751f565099318181aa
Chiptune <modchiptune@gmail.com>
2019-01-25 15:05:34 +0900

    LIST

--- team.js
+++ team.js
@@ -13,35 +13,25 @@ function xs(a){
 	}}
 	function err(e){a.call(x,e)}
 }
-function rs(t,cb){
-	var c,s=(t||document).querySelectorAll("script"),l=s.length;
-	r1(0);
-	function r1(i){if(i<l){
-		c=document.createElement("script");
-		if(s[i].src){
-			c.src=s[i].src;
-			c.async=true;
-			c.onload=function(){
-				r1(i+1);
-			};
-		}else{
-			c.text=s[i].text;
-		}
-		s[i].parentNode.insertBefore(c,s[i]);
-		s[i].remove();
-		if(c.text)r1(i+1);
-	}else if(cb)cb()}
-}
 (function(){
 var x=xs(function(){
-	var m,s=this.responseText,d=document.createElement("div");
+	var m,s=this.responseText,d=document.createElement("div"),c=c=d.children;
 	m=/<body>([\w\W]*)<\/body>/.exec(s);
 	document.body.innerHTML=m[1];
 	m=/(<script[\w\W]*)<\/head>/.exec(s);
 	d.innerHTML=m[1];
-	rs(d,function(){
-		for(var i=d.children.length;i>0;i--)document.head.appendChild(d.children[0]);
-	});
+	r1();
+	function r1(){
+		if(!c.length){
+			for(var i=d.children.length;i>0;i--)document.head.appendChild(d.children[0]);
+			return;
+		}
+		var a=document.createElement("script");
+		a.src=c[0].src;
+		a.onload=r1;
+		d.removeChild(c[0]);
+		document.head.appendChild(a);
+	}
 });
 x.open("GET","/",true);
 x.send();
